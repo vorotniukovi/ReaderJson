f86fe4ce74933995dbbf4c4d9000061c972968b8
Vorotnikov Igor <vorotnikov_i@mail.ru>
2019-03-31 09:14:30 +0300

    Reader big json files. Test lib.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?//require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+<?
+set_time_limit(0);
+ini_set('memory_limit', '2056M');
+use dump_r\Core;
+
+$jsonStream = \JsonMachine\JsonMachine::fromFile("data.json");
+foreach ($jsonStream as $name) {
+    echo "<pre>"; print_r($name); echo "</pre>";
+}
+?>
+<?//require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
